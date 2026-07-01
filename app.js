b10a8e02b42ec23eb7dd5cb3b44fd2c9dba50de4
arfinaleusuf <arfinaleusuf@gmail.com>
2026-07-02 00:39:52 +0600

    Add box elements and styling for layout and interactivity

--- app.js
+++ app.js
@@ -2,3 +2,13 @@
 const target = document.getElementById("title")
 
 target.style.color = "red";
+
+const allBox = document.getElementsByClassName("box");
+
+for(let i = 0; i<allBox.length; i++){
+    const element = allBox[i];
+    element.style.backgroundColor = "green";
+    if(element.innerText == "box-5"){
+        element.style.backgroundColor = "red"
+    }
+}
