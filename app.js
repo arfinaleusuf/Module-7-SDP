cbddad6cbf33d5f266474c3ef0583dd1bfc9896b
arfinaleusuf <arfinaleusuf@gmail.com>
2026-07-13 00:00:27 +0600

    Update displayData function to include user details structure

--- app.js
+++ app.js
@@ -50,5 +50,11 @@ const displayData = (userData) => {
 
     userData.forEach(user => {
         const div = document.createElement("div");
+        div.innerHTML = `
+        <h4> title </h4>
+        <p> </p>
+        <button>Details</button>
+        `
+        container.appendChild(div);
     });
 }
