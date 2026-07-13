89b7373def49dc6ae03e33bd0dd795f41c9e8653
arfinaleusuf <arfinaleusuf@gmail.com>
2026-07-14 00:13:53 +0600

    data call

--- project.js
+++ project.js
@@ -1,5 +1,29 @@
 const loadAllProduct = () => {
     fetch('https://fakestoreapi.com/products')
-        .then(response => response.json())
-        .then(data => console.log(data));
+        .then(res => res.json())
+        .then((data) => {
+            displayProduct(data)
+        });
 }
+
+const displayProduct = (products) => {
+    const productContainer = document.getElementById("product-container")
+    products.forEach(product => {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+        <img src = ""/>
+        <h5>h</h5>
+        <p></p>
+        <button>details</button>
+        <button>add to cart</button>
+        `;
+        productContainer.appendChild(div)
+    });
+}
+
+
+
+
+
+loadAllProduct()
